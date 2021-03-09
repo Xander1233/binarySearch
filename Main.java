7c1882eafe4f4df9837b987b00fc1af9a62ecce8
Xandrrrr <57237934+Xander1233@users.noreply.github.com>
2021-03-09 19:56:16 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
         int[] array = { 1, 3, 5, 7, 8, 10, 11, 13, 14, 16, 19 };
 
-        int startNumber = array.length - 1;
+        int left = 0,
+            right = array.length - 1,
+            expectedResult = 7;
 
-        int expectedResult = 7;
+        boolean hasFound = false;
 
-        while (array[startNumber] != expectedResult)
-            if (array[startNumber] > expectedResult) startNumber = startNumber >> 1; // Half the startNumber
-            else if (array[startNumber] < expectedResult) startNumber += startNumber >> 1; // Add the half of the startNumber to the old startNumber (startNumber = 10; startNumber >> 1 = 5; 10 + 10 >> 1 == 15)
+        while (left <= right && !hasFound) {
+            int mid = (int) Math.floor((left + right) >> 1);
+            if (array[mid] < expectedResult)
+                left = mid + 1;
+            else if (array[mid] > expectedResult)
+                right = mid - 1;
+            else
+                hasFound = true;
+        }
 
-        System.out.println(array[startNumber]); // Print out the result
-
+        System.out.println(hasFound);
     }
 }
