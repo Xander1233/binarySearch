1e302caab411e2559345fea7b9ddb957e0fdb5e6
Xandrrrr <57237934+Xander1233@users.noreply.github.com>
2021-03-09 11:24:59 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] aufstellung = { 1, 3, 5, 7, 8, 10, 11, 13, 14, 16, 19 };
+        int[] array = { 1, 3, 5, 7, 8, 10, 11, 13, 14, 16, 19 };
 
-        int start = aufstellung.length - 1;
+        int startNumber = array.length - 1;
 
-        int ergebnis = 7;
+        int expectedResult = 7;
 
-        while (aufstellung[start] != ergebnis)
-            if (aufstellung[start] > ergebnis) start = start >> 1;
-            else if (aufstellung[start] < ergebnis) start += start >> 1;
+        while (array[startNumber] != expectedResult)
+            if (array[startNumber] > expectedResult) startNumber = startNumber >> 1; // Half the startNumber
+            else if (array[startNumber] < expectedResult) startNumber += startNumber >> 1; // Add the half of the startNumber to the old startNumber (startNumber = 10; startNumber >> 1 = 5; 10 + 10 >> 1 == 15)
 
-        System.out.println(aufstellung[start]);
+        System.out.println(array[startNumber]); // Print out the result
 
     }
 }
